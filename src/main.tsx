402ebca09f11ff571b3e448254890c69b3850da6
Henry <huzo93.hu@gmail.com>
2026-04-30 17:02:19 +0100

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./styles/index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient.ts";
 import { HelmetProvider } from "react-helmet-async";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <QueryClientProvider client={queryClient}>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </QueryClientProvider>,
 );
