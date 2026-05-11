ecf6ae5fddda2ace894929ae8eb2b6c828641949
Uzochukwu Emeka <uzochukwuemeka516@gmail.com>
2026-05-11 15:27:01 -0700

    update

--- src/app/assests/videos/index.ts
+++ src/app/assests/videos/index.ts
@@ -1,5 +1,5 @@
 import promoVideo from "./video2.mp4";
-import vet from "./vet.mp4";
+import vet from "./video3.mp4";
 import sadDog from "./sad-dog.mp4";
 import bigVideo from "./big-video.mp4"
 import catDogVideo from "./cat-dog-video.mp4"
